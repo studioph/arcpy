ae914226f7bec63f6a53995669fb3ac4feb20937
Paul Hutchings <dev@studiop.page>
2024-06-01 10:13:57 -0400

    Add code and tests

---
+++ __test__/archive_test.py
@@ -0,0 +1,105 @@
+from os import PathLike, fspath
+from pathlib import Path
+
+import pytest
+
+from arcpy import archive, util
+
+TEST_DATA_FOLDER = Path(__file__).parent / "data"
+
+
+@pytest.mark.parametrize(
+    ("file", "expected"),
+    [
+        (Path("/foo/bar/baz.zip"), "baz"),
+        (Path("foo.part1.rar"), "foo"),
+        (Path("foo.bar.part01.tar.gz"), "foo.bar"),
+    ],
+)
+def test_get_archive_name(file: Path, expected: str):
+    assert util.get_archive_name(file) == expected
+
+
+def test_rar(tmp_path: Path):
+    with archive.RarArchive(TEST_DATA_FOLDER / "test.rar") as rar:
+        rar.extract("test", dest=tmp_path)
+    assert tmp_path.joinpath("test").exists()
+
+
+def test_encrypted_rar_throws(tmp_path: Path):
+    with (
+        pytest.raises(archive.ArchiveError),
+        archive.RarArchive(TEST_DATA_FOLDER / "test-pwd.rar") as rar,
+    ):
+        rar.extract("test", dest=tmp_path)
+
+
+def test_zip(tmp_path: Path):
+    with archive.ZipArchive(TEST_DATA_FOLDER / "test.zip") as z:
+        z.extract("test", dest=tmp_path)
+    assert tmp_path.joinpath("test").exists()
+
+
+def test_encrypted_zip_throws(tmp_path: Path):
+    with (
+        pytest.raises(archive.ArchiveError),
+        archive.ZipArchive(TEST_DATA_FOLDER / "test-pwd.zip") as z,
+    ):
+        z.extract("test", dest=tmp_path)
+
+
+def test_tar(tmp_path: Path):
+    with archive.TarArchive(TEST_DATA_FOLDER / "test.tar.gz") as tar:
+        tar.extract("test", dest=tmp_path)
+    assert tmp_path.joinpath("test").exists()
+
+
+def test_tar_throws(tmp_path: Path):
+    with (
+        pytest.raises(archive.ArchiveError),
+        archive.TarArchive(TEST_DATA_FOLDER / "test.tar.gz") as tar,
+    ):
+        tar.extract("foo", dest=tmp_path)
+
+
+@pytest.mark.parametrize(
+    ("file",),
+    [
+        (TEST_DATA_FOLDER / "test.zip",),
+        (TEST_DATA_FOLDER / "test.rar",),
+        (fspath(TEST_DATA_FOLDER / "test.tar.gz"),),
+        (TEST_DATA_FOLDER / "test.copy.tar.gz",),
+    ],
+)
+def test_open(file: PathLike):
+    archive.open(file)
+
+
+@pytest.mark.parametrize(
+    ["file", "parts"],
+    [
+        (
+            TEST_DATA_FOLDER / "multipart.part01.rar",
+            [
+                TEST_DATA_FOLDER / "multipart.part01.rar",
+                TEST_DATA_FOLDER / "multipart.part02.rar",
+            ],
+        ),
+        (
+            TEST_DATA_FOLDER / "multipart.part1.zip",
+            [
+                TEST_DATA_FOLDER / "multipart.part1.zip",
+                TEST_DATA_FOLDER / "multipart.part2.zip",
+            ],
+        ),
+    ],
+)
+def test_multipart(file: PathLike, parts: list[Path]):
+    arcfile = archive.open(file)
+    assert arcfile.parts == parts
+
+
+def test_unknown_type():
+    file = TEST_DATA_FOLDER / "bad.zip.foo"
+    with pytest.raises(archive.ArchiveError):
+        archive.open(file)
---
+++ src/arcpy/archive.py
@@ -0,0 +1,160 @@
+import logging
+import pathlib
+import tarfile
+import zipfile
+from abc import ABC, abstractmethod
+from os import PathLike, fspath
+from types import TracebackType
+from typing import Generic, Iterable, Type, TypeVar
+
+from unrar import rarfile
+
+from . import util
+
+LOG = logging.getLogger(__name__)
+
+
+T = TypeVar("T")
+
+
+class ArchiveError(Exception):
+    """Aggregates errors from different archive types"""
+
+
+REGISTRY = {}
+
+
+def register(cls, *extensions: str):
+    """Registers a new subclass implementation and its associated file extensions"""
+    LOG.debug(
+        "Registered new Archive implementation: %s with extensions %s", cls, extensions
+    )
+    for ext in extensions:
+        REGISTRY[ext] = cls
+
+
+class Archive(Generic[T], ABC):
+    _archive: Type[T]
+
+    def __init_subclass__(cls, /, extensions: Iterable[str]) -> None:
+        register(cls, *extensions)
+
+    def __init__(self, filepath: PathLike) -> None:
+        self.parts = [pathlib.Path(filepath)]
+        self.name = util.get_archive_name(self.parts[0])
+        self.multipart = "part" in self.parts[0].name
+        if self.multipart:
+            self._get_parts()
+
+    def __str__(self) -> str:
+        return str(self.parts[0].parent / self.name)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}('{self}')"
+
+    def __enter__(self):
+        self._archive.__enter__()
+        return self
+
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> bool | None:
+        self._archive.__exit__(__exc_type, __exc_value, __traceback)
+
+    @property
+    @abstractmethod
+    def items(self) -> Iterable[PathLike]: ...
+
+    @abstractmethod
+    def extract_items(self, items: Iterable[PathLike], dest: PathLike): ...
+
+    def extract(self, item: PathLike, dest: PathLike):
+        self.extract_items(items=[item], dest=dest)
+
+    def move(self, target: pathlib.Path):
+        if not target.is_dir():
+            raise ValueError("Target must be a folder")
+        self.parts = [part.rename(target / part.name) for part in self.parts]
+
+    def _get_parts(self):
+        folder = self.parts[0].parent
+        self.parts = list(folder.glob(f"{self.name}*{self.parts[0].suffix}"))
+        LOG.debug("%s is multipart archive with parts %s", self, self.parts)
+
+
+class ZipArchive(Archive[zipfile.ZipFile], extensions=(".zip",)):
+
+    def __init__(self, filepath: PathLike) -> None:
+        with util.reraise(zipfile.BadZipFile, as_=ArchiveError):
+            super().__init__(filepath)
+            self._archive = zipfile.ZipFile(file=filepath)
+
+    @property
+    def items(self) -> Iterable[PathLike]:
+        return self._archive.namelist()
+
+    def extract_items(self, items: Iterable[PathLike], dest: PathLike):
+        with util.reraise(RuntimeError, as_=ArchiveError):
+            return self._archive.extractall(members=items, path=dest)
+
+
+class TarArchive(
+    Archive[tarfile.TarFile],
+    extensions=(
+        ".tar",
+        ".tar.gz",
+        ".tgz",
+        ".tar.bz2",
+        ".tbz2",
+        ".tar.bz",
+        ".tbz",
+        ".tar.xz",
+        ".txz",
+    ),
+):
+
+    def __init__(self, filepath: PathLike) -> None:
+        with util.reraise(
+            tarfile.ReadError, tarfile.CompressionError, as_=ArchiveError
+        ):
+            super().__init__(filepath)
+            self._archive = tarfile.open(filepath)
+
+    @property
+    def items(self) -> Iterable[PathLike]:
+        return self._archive.getnames()
+
+    def extract_items(self, items: Iterable[PathLike], dest: PathLike):
+        with util.reraise(KeyError, as_=ArchiveError):
+            tarinfos = [self._archive.getmember(item) for item in items]
+            return self._archive.extractall(members=tarinfos, path=dest)
+
+
+class RarArchive(Archive[rarfile.RarFile], extensions=(".rar",)):
+
+    def __init__(self, filepath: PathLike) -> None:
+        with util.reraise(rarfile.BadRarFile, as_=ArchiveError):
+            super().__init__(filepath)
+            self._archive = rarfile.RarFile(filename=fspath(filepath))
+
+    @property
+    def items(self) -> Iterable[PathLike]:
+        return self._archive.namelist()
+
+    def extract_items(self, items: Iterable[PathLike], dest: PathLike):
+        with util.reraise(RuntimeError, as_=ArchiveError):
+            return self._archive.extractall(members=items, path=fspath(dest))
+
+
+def open(filepath: PathLike) -> Archive:  # pylint: disable=redefined-builtin
+    path = pathlib.Path(filepath)
+    LOG.debug("Attempting to create archive object from %r", path.name)
+    for i in range(len(path.suffixes)):
+        suffix = "".join(path.suffixes[i:])
+        if registry_entry := REGISTRY.get(suffix):
+            return registry_entry(path)
+
+    raise ArchiveError(f"No archive type found in registry for {path.name}")
